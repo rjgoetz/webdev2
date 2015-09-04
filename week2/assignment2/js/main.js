2678b921fec7a31376cfec7ef19d51c590755c19
rjgoetz <rj@rjgoetz.com>
2015-09-04 12:09:32 -0500

    updated style; added j/s

---
+++ week2/assignment2/js/main.js
@@ -0,0 +1,35 @@
+var al = document.getElementById("al");
+var alFunction = function () {
+  alert("The state capital of Alabama is Montgomery");
+};
+al.addEventListener("mouseover", alFunction);
+
+var ak = document.getElementById("ak");
+var akFunction = function () {
+  alert("The state capital of Alaska is Juneau");
+};
+ak.addEventListener("mouseover", akFunction);
+
+var az = document.getElementById("az");
+var azFunction = function () {
+  alert("The state capital of Arizona is Phoenix");
+};
+az.addEventListener("mouseover", azFunction);
+
+var ar = document.getElementById("ar");
+var arFunction = function () {
+  alert("The state capital of Arkansas is Little Rock");
+};
+ar.addEventListener("mouseover", arFunction);
+
+var ca = document.getElementById("ca");
+var caFunction = function () {
+  alert("The state capital of California is Sacramento");
+};
+ca.addEventListener("mouseover", caFunction);
+
+var co = document.getElementById("co");
+var coFunction = function () {
+  alert("The state capital of Colorado is Denver");
+};
+co.addEventListener("mouseover", coFunction);
