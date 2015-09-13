fd523b995baf4af5700d5930e83f4a0ba57b49bc
rjgoetz <rj@rjgoetz.com>
2015-09-13 07:56:06 -0500

    assignment 3 files, linked javascript file, added code

---
+++ week3/assignment3/script.js
@@ -0,0 +1,9 @@
+/*
+  Don't worry about the getElementById or addEventListener commands at this point,
+  we'll cover them more in-depth later in the course.
+  For the purposes of this assignment, you only need to understand that these commands
+  are used to respond to user interaction with the "Donate" button.
+*/
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  console.log("Hello World!");
+});
