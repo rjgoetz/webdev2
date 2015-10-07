dca13a57f402cb4bae70531d769c6eb565bd066a
RJ Goetz <rj@rjgoetz.com>
2015-10-07 11:09:36 -0500

    exercise7.2 files

---
+++ week7/exercise7.2/main.js
@@ -0,0 +1,21 @@
+// main.js
+var boxElement = document.getElementById('box'),
+	defaultBox = 'default-box',
+	colorBox = ['orange', 'blue', 'yellow'],
+	currentColor = 0
+;
+
+boxElement.onclick = function() {
+	if (boxElement.className === defaultBox + ' ' + colorBox[0]) {
+		currentColor++;
+		console.log(currentColor);
+		boxElement.className = defaultBox + ' ' + colorBox[1];		
+	} else if (currentColor === 1) {
+		currentColor++;
+		console.log(currentColor);
+		boxElement.className = defaultBox + ' ' + colorBox[2];	
+	} else if (currentColor === 2) {
+		currentColor = 0;
+		boxElement.className = defaultBox + ' ' + colorBox[0];
+	}
+};
