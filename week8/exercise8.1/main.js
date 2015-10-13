7e6e6cd1e277bd1069705f8dc9a4a825197d415f
rjgoetz <rj@rjgoetz.com>
2015-10-12 20:24:23 -0500

    updated js function names

--- week8/exercise8.1/main.js
+++ week8/exercise8.1/main.js
@@ -6,28 +6,28 @@ var h1 = document.getElementsByTagName("h1"),
     buttons = [false, false];
 
 // returns number of elements of same type on page
-function loopElement(element) {
+function numElements(element) {
   var num = element.length;
   return num;
 };
 
 // adds ".red" class to element
 function colorRed(element) {
-  for (var i = 0; i < loopElement(element); i++) {
+  for (var i = 0; i < numElements(element); i++) {
     element[i].className = "red";
   };
 };
 
 // adds ".blue" class to element
 function colorBlue(element) {
-  for (var i = 0; i < loopElement(element); i++) {
+  for (var i = 0; i < numElements(element); i++) {
     element[i].className = "blue";
   };
 };
 
 // removes class from element
 function colorOff(element) {
-  for (var i = 0; i < loopElement(element); i++) {
+  for (var i = 0; i < numElements(element); i++) {
     element[i].className = "";
   };
 };
