bc48cbbce2708bfaeb5e7f6bb4f4f02f752beb40
rjgoetz <rj@rjgoetz.com>
2015-10-29 16:18:17 -0500

    added specialsLinks solution

--- week10/exercise10.2/main.js
+++ week10/exercise10.2/main.js
@@ -1,5 +1,24 @@
 // main.js
 
+function createSpecialsLinks() {
+	$("#specials h2").clone().insertAfter("#specials h1").contents().unwrap().wrapAll("<ul></ul>").wrap("<li></li>").wrap("<a></a>");
+}
+
+function addAnchorTags() {
+	var i = 1;
+	$("#specials a").each(function() {
+		$(this).prop("href", "#" + i);
+		i++;
+	})
+	i = 1;
+	$("#specials h2").each(function() {
+		$(this).prop("id", i);
+		i++;
+	})
+}
+
 $("document").ready(function() {
 	$("#restaurants li[data-type='veg']").append(" (v)");
+	createSpecialsLinks();
+	addAnchorTags();
 });
