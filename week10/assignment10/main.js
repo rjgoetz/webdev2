53452c49feeefe100e8dc3bc56893263b63496c4
RJ Goetz <rj@rjgoetz.com>
2015-10-30 13:26:54 -0500

    added drop nav

--- week10/assignment10/main.js
+++ week10/assignment10/main.js
@@ -1,5 +1,34 @@
 // main.js
 
+//add dropdown menu for products and services
+
+function dropdownNav() {
+
+	var productsLinks = ["Lorem", "Ipsum", "Radish"];
+	var servicesLinks = ["Dolor", "Volutpat", "Harvesting"];
+
+	//create drop-down container
+	var dropDown = $("<ul></ul>").prop("class", "drop");
+	$(".drop-down a").append(dropDown);
+
+	//add products links
+	for (var i = 0; i<productsLinks.length; i++) {
+		$(".drop-down:eq(0) a ul").append("<li><a href='#'>" + productsLinks[i] + "</li></a>");
+	}
+
+	//add services links
+	for (var i = 0; i<servicesLinks.length; i++) {
+		$(".drop-down:eq(1) a ul").append("<li><a href='#'>" + servicesLinks[i] + "</li></a>");
+	}
+
+	//hover unhides products menu
+	$(".drop-down").hover(function() {
+		$(this).find(".drop").show();
+	}, function() {
+		$(this).find(".drop").hide();
+	});
+}
+
 $("document").ready(function() {
 	// add background color to a elements
 	$("a").click(function() {
@@ -9,4 +38,5 @@ $("document").ready(function() {
 	$("h2.title:eq(1)").click(function() {
 		$(this).remove();
 	})
+	dropdownNav();
 });
