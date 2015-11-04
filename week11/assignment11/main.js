563a78792c2e700ccf59ab44b86a9e0e48617f6e
RJ Goetz <rj@rjgoetz.com>
2015-11-04 08:51:14 -0600

    add files; link scripts; begin jQuery

---
+++ week11/assignment11/main.js
@@ -0,0 +1,15 @@
+// main.js
+
+
+// create icon object and prepend to anchor text
+var iconImage = $("<img src='icon.png'>").css("margin-right", "10px");
+$("#menu li a").prepend(iconImage);
+
+// click H1 in header to change header background
+
+
+
+
+$("document").ready(function () {
+	
+});
