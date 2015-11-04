d4502dcbcdc6389ad46eec0b67e1e84842561133
RJ Goetz <rj@rjgoetz.com>
2015-11-04 09:56:14 -0600

    added script for changing header background color

--- week11/assignment11/main.js
+++ week11/assignment11/main.js
@@ -2,14 +2,27 @@
 
 
 // create icon object and prepend to anchor text
-var iconImage = $("<img src='icon.png'>").css("margin-right", "10px");
-$("#menu li a").prepend(iconImage);
+function addIconImage() {
+	var iconImage = $("<img src='icon.png'>").css("margin-right", "10px");
+	$("#menu li a").prepend(iconImage);
+};
 
-// click H1 in header to change header background
-
-
+// click H1 to toggle header background color
+function changeHeaderBackground() {
+	var i = true;
+	$("#header h1").click(function() {
+		if(i) {
+			$("#header").css("background", "#FFCC66");
+			i = false;
+		} else {
+			$("#header").css("background", "#699C4D");
+			i = true;
+		}
+	})
+};
 
 
 $("document").ready(function () {
-	
+	addIconImage();
+	changeHeaderBackground();
 });
