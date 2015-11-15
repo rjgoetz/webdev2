6f891756a69fde8e60410f76925074f12b625d63
rjgoetz <rj@rjgoetz.com>
2015-11-15 07:38:47 -0600

    fixed bug when clicking on current recipe link; added active link class

--- week12/assignment12/main.js
+++ week12/assignment12/main.js
@@ -7,36 +7,49 @@ function recipeNav() {
 
 	// loop through h2 array and append text and id# to navigation
 	for (var i = 0; i < $h2.length; i++) {
-		$("nav ul").append("<li><a href='#'>" + $h2.eq(i).text() + "</a></li");
+		$("nav ul").append("<li>" + $h2.eq(i).text() + "</li");
 	}
+
+	// add style class to active link
+	$("nav li:first-of-type").addClass("active");
 };
 
 // change recipe on navigation click
 function fadeInRecipe() {
-	$("nav a").click(function() {
+	$("nav li").click(function() {
 		// store click index
-		var linkIndex = $("nav a").index(this);
+		var linkIndex = $("nav li").index(this);
 		// store .previous index
 		var previousClassIndex = $("div .previous").index();
-		console.log(previousClassIndex);
 
-		// add .previous class to "currently viewed recipe"
-		$("article:eq("+previousClassIndex+")").addClass("previous");
+		// run animation when switching to new recipe
+		if (linkIndex === previousClassIndex) {
+			// do nothing
+		} else {
+			// remove active link class
+			$("nav li").removeClass("active");
 
-		// add .next class to clicked recipe
-		$("article:eq("+linkIndex+")").addClass("next");
+			// add active link class
+			$("nav li:eq("+linkIndex+")").addClass("active");
 
-		// fade out currently viewed recipe
-	  $("article:eq("+previousClassIndex+")").animate({
-			opacity: 0,
-		}, 1000, function() {
-			// reset classes
-			$("article:eq("+linkIndex+")").removeClass("next").addClass("previous");
-			$("article:eq("+previousClassIndex+")").removeClass("previous");
+			// add .previous class to "currently viewed recipe"
+			$("article:eq("+previousClassIndex+")").addClass("previous");
 
-			// reset opacity for all recipes to 1
-			$("article").css("opacity", "1");
-		});
+			// add .next class to clicked recipe
+			$("article:eq("+linkIndex+")").addClass("next");
+
+			// fade out currently viewed recipe
+		  $("article:eq("+previousClassIndex+")").animate({
+				opacity: 0,
+			}, 1000, function() {
+				// reset classes
+				$("article:eq("+linkIndex+")").removeClass("next").addClass("previous");
+				$("article:eq("+previousClassIndex+")").removeClass("previous");
+
+				// reset opacity for all recipes to 1
+				$("article").css("opacity", "1");
+			});
+		}
 	})
 };
 
